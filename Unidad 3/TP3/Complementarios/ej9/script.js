5b87517cf2bf587517deb5c4d74aed7e6d6b753c
dimidio6 <angelo.d6@outlook.com>
2025-05-29 22:10:28 -0300

    update

--- Unidad 3/TP3/Complementarios/ej9/script.js
+++ Unidad 3/TP3/Complementarios/ej9/script.js
@@ -15,29 +15,59 @@ document.addEventListener("DOMContentLoaded", () => {
             hijos.forEach(hijo => {
                 if (hijo.className == "producto") {
                     nombre_producto = hijo.innerText;
-                    console.log(nombre_producto);
+                    // console.log(nombre_producto);
                 }
                 else if (hijo.className == "precio") {
                     precio_producto = hijo.innerText;
-                    console.log(precio_producto);
+                    // console.log(precio_producto);
                 }
             })
-            agregar_producto(nombre_producto,precio_producto);
+            agregar_producto(nombre_producto, precio_producto);
         })
     });
 
-    function agregar_producto (nombre, precio) {
-       let tabla = document.getElementById("tabla");
-       let fila = document.createElement("tr");
-       let col1 = document.createElement("td");
-       let col2 = document.createElement("td");
-       let col3 = document.createElement("td");
-       col1.innerText = nombre;
-       col1.className = "clase"+nombre; // a cada elemento que agrego en "Producto" le asigno una clase "clase_<nombre del producto>"
-       col3.innerText = precio;
-       fila.appendChild(col1);
-    //    fila.appendChild(col2); // falta hacer el contador
-       fila.appendChild(col3);
-       tabla.appendChild(fila);
+    function agregar_producto(nombre, precio) {
+        let tabla = document.getElementById("tabla_cuerpo"); // <tbody> de la tabla
+        let filas = tabla.childNodes;
+        console.log(filas);
+        if (filas.length == 0) { // comprueba si el <tbody> está vacío
+            console.log("crea tbody")
+            let fila = document.createElement("tr");
+
+            let col_produ = document.createElement("td"); // columna para el producto
+            col_produ.setAttribute("class", nombre); // le asigno como nombre de su clase el mismo nombre del producto ej: <td class="Pantalon">Pantalon</td>
+            let col_cant = document.createElement("td"); // columna para la cantidad
+            let col_import = document.createElement("td"); // columna para el importe
+
+            col_produ.innerText = nombre;
+
+            let parrafo = document.createElement("p");
+            parrafo.innerHTML = parseInt(1);
+            col_cant.appendChild(parrafo);
+
+            col_import.innerText = precio;
+            // agrego las 3 columnas a la fila
+            fila.appendChild(col_produ);
+            fila.appendChild(col_cant);
+            fila.appendChild(col_import);
+            tabla.appendChild(fila); // agrego la fila al <tbody> de la tabla
+        }
+        else {
+            filas.forEach(fila => {
+                // console.log(fila);
+                let columnas = fila.childNodes; // los hijos de la fila (o sea las columnas)
+                    columnas.forEach(columna => { // itero sobre ellos
+                        if (columna.className == nombre) { // si ya existe un Producto con el mismo nombre en la tabla
+                            let col_cantidad = columna.nextElementSibling; // el hermano de la columna del producto (o sea la columna cantidad)
+                            cantidad = col_cantidad.firstChild // le asigno su hijo (el párrafo dentro de la celda de cantidad)
+                            console.log(col_cantidad);
+                            console.log(cantidad);
+                            console.log(cantidad.getAttribute("value"));
+                            // parseInt(cantidad) = cantidad + 1;
+                            // columna.nextSibling = columna.nextSibling + 1;
+                        }
+                    })
+                })
+        }
     }
 })
